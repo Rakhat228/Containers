675504a56c41ba11bdb24aad26000943022baa63
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2023-07-04 13:10:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ log_title = st.text_input('Login')
 log_pass = st.text_input('password') 
 
 if log_title == login and log_pass == password:
+    st.multiselect(label, options)
     number = st.number_input('number', min_value=0, step=1)
     st.write(number)
     for i in range(1, number+1):
