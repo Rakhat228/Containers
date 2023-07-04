9f292d5ee6f1417001c7d432ccdb2acf26ccdb7f
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2023-07-04 10:07:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-
-login = 'a'
+df = []
+prev_num = 1001581234
+login = 'Kalykova'
 password = '12345678'
-
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
-df = load_data(st.secrets["public_gsheets_url"])
-st.write(df)
-
 log_title = st.text_input('Login')
-log_pass = st.text_input('password')   
-
-number = st.number_input('number', min_value=0, step=1)
+log_pass = st.text_input('password') 
 
 if log_title == login and log_pass == password:
+    number = st.number_input('number', min_value=0, step=1)
     st.write(number)
+    for i in range(1, number):
+        df.append(prev_num + i)
+    st.write(df)
+
+
+
