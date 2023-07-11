1a2236ae342e5640986d449ed5285ac31186e813
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2023-07-11 12:15:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ xd = pd.read_csv(csv_url)
 
 log_title = st.text_input('Login')
 log_pass = st.text_input('password') 
-options = {'Mocha':'mocha', 'Kal':'Kal', 'Soskob':'soskob'}
+options = {'Пробирка со средой Кэри Блера':'337454118','Mocha':'mocha', 'Kal':'Kal', 'Soskob':'soskob'}
 
 
 for i in range(len(xd['Login'])):
@@ -23,6 +23,7 @@ for i in range(len(xd['Login'])):
         token = xd['Token'][i]
         st.write(token)
         st.selectbox('Выбрать контейнер', options)
+        URL = "https://docs.google.com/spreadsheets/d/1I7cU0ZPlL5YatiAI5qMN7ZAe1qEmeRvwWz85go-bVTs/edit#gid="+option
         number = st.number_input('number', min_value=0, step=1)
         st.write(number)
         for i in range(1, number+1):
