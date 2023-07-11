9ada91d385189b440dd619e28e5dd8e8875c3ebd
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2023-07-11 11:56:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,45 +8,39 @@ st.subheader('Cnt')
 lst = []
 prev_num = 1001581234
 
-#def load_data(sheets_url):
 sheet_url = "https://docs.google.com/spreadsheets/d/1I7cU0ZPlL5YatiAI5qMN7ZAe1qEmeRvwWz85go-bVTs/edit#gid=0"
 csv_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
 xd = pd.read_csv(csv_url)
 st.write(xd)
 
-#df = load_data(st.secrets["public_gsheets_url"])
-
-login = 'Kalykova'
-password = '12345678'
 log_title = st.text_input('Login')
 log_pass = st.text_input('password') 
 options = {'Mocha':'mocha', 'Kal':'Kal', 'Soskob':'soskob'}
 
 
-
-
-
-if log_title == login and log_pass == password:
-    st.selectbox('Выбрать контейнер', options)
-    number = st.number_input('number', min_value=0, step=1)
-    st.write(number)
-    for i in range(1, number+1):
-        lst.append(prev_num + i)
-    df = pd.DataFrame()
-    df['Уникальный штрих-код контейнера'] = lst
-    st.write(df)
-
-    def to_excel(df):
-       output = BytesIO()
-       writer = pd.ExcelWriter(output, engine='openpyxl')
-       df.to_excel(writer, index=False, sheet_name='Sheet1') 
-       writer.close()
-       processed_data = output.getvalue()
-       return processed_data
-    df_xlsx = to_excel(df)
-    st.download_button(label='📥 Скачать готовый файл',
-                                   data = df_xlsx ,
-                                   file_name= 'Output.xlsx')
+for i in range(len(xd['Login']):
+    if log_title == xd['Login'][i] and log_pass == xd['Password'][i]:
+        st.write(token)
+        st.selectbox('Выбрать контейнер', options)
+        number = st.number_input('number', min_value=0, step=1)
+        st.write(number)
+        for i in range(1, number+1):
+            lst.append(prev_num + i)
+        df = pd.DataFrame()
+        df['Уникальный штрих-код контейнера'] = lst
+        st.write(df)
+    
+        def to_excel(df):
+           output = BytesIO()
+           writer = pd.ExcelWriter(output, engine='openpyxl')
+           df.to_excel(writer, index=False, sheet_name='Sheet1') 
+           writer.close()
+           processed_data = output.getvalue()
+           return processed_data
+        df_xlsx = to_excel(df)
+        st.download_button(label='📥 Скачать готовый файл',
+                                       data = df_xlsx ,
+                                       file_name= 'Output.xlsx')
         
 
 
