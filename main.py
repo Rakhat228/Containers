0a989fe9f02d8e0a75e051754df4c3f3c20653f7
Rakhat <81379577+Rakhat228@users.noreply.github.com>
2023-08-08 10:02:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ for i in range(len(xd['Login'])):
         st.download_button(label='📥 Скачать готовый файл',
                                        data = df_xlsx ,
                                        file_name= 'Output.xlsx')
-    else:
-       st.write('Неправильный логин или пароль')
-       break
+   # else:
+     #  st.write('Неправильный логин или пароль')
+       #break
         
         
 
